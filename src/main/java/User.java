27ea98f1f65c4c84c9c7cebca279789dad8963a5
daniel <dantov87@hotmail.com>
2015-04-01 13:13:30 +0200

    OT-2 - my rebase squash to one commit

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -4,4 +4,24 @@
 public class User {
     private String name;
     private String lastName;
+
+    public User(){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
